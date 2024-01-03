38f845c998127b15cb88f879960ef85a950206c5
leiying666 <148442801+leiying666@users.noreply.github.com>
2024-01-03 21:45:06 +0800

    Create 通讯录——contact.h

---
+++ 通讯录——contact.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<stdio.h>
+#include<string.h>
+#define MAX 1000
+#define MAX_NAME 10
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+//创建存放一个人信息的结构体元素
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo data[MAX];//含有MAX个struct PeoInfo结构体元素
+	int size;
+};
+//函数声明
+void InitContact(struct Contact* ps);
+void AddContact(struct Contact* ps);
+void ShowContact(const struct Contact* ps);
